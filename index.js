067b98f548361a6e0201238ddd670f1698a3328b
Andrew Chen <ac12491@gmail.com>
2017-05-31 02:34:08 -0600

    added public folder

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const app = module.exports = express();
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
 
 app.use(express.static(__dirname));
 
